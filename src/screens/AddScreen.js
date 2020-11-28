7af1de47efc5ad4bd7f2adb4e9bacb621c2d4024
darrell bazian <darrellbazianjr@gmail.com>
2020-11-28 17:04:38 -0500

    adding validation to add client form

--- src/screens/AddScreen.js
+++ src/screens/AddScreen.js
@@ -3,38 +3,71 @@ import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { useDispatch } from "react-redux";
 import { addClient } from "../../store/actions/clientListActions";
 import ClientModel from "../../models/ClientModel";
+import { useForm, Controller } from "react-hook-form";
 
 const AddScreen = () => {
+  const { control, handleSubmit, errors } = useForm();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [address, setAddress] = useState("");
   const [city, setCity] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
-
+  const onSubmit = (data) => console.log(data);
   const dispatch = useDispatch();
-
   const clientList = new ClientModel(firstName, lastName, address, city, phoneNumber, email);
-  const addClientHandler = () => {
-    dispatch(addClient(clientList));
-  };
 
   return (
     <View style={styles.full}>
-      <TextInput
-        value={firstName}
-        onChangeText={(e) => setFirstName(e)}
-        style={styles.textInput}
-        placeholder={"First Name"}
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            value={value}
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            style={styles.textInput}
+            placeholder={"First Name"}
+          />
+        )}
+        name="firstName"
+        rules={{ required: true }}
+        defaultValue=""
       />
-      <TextInput
-        value={lastName}
-        onChangeText={(e) => setLastName(e)}
-        style={styles.textInput}
-        placeholder={"Last Name"}
+      {errors.firstName && <Text style={styles.errorText}>This is required.</Text>}
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            value={value}
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            style={styles.textInput}
+            placeholder={"Last Name"}
+          />
+        )}
+        name="lastName"
+        rules={{ required: true }}
+        defaultValue=""
       />
+      {errors.firstName && <Text style={styles.errorText}>This is required.</Text>}
       <TextInput value={address} onChangeText={(e) => setAddress(e)} style={styles.textInput} placeholder={"Address"} />
-      <TextInput value={city} onChangeText={(e) => setCity(e)} style={styles.textInput} placeholder={"City"} />
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            value={value}
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            style={styles.textInput}
+            placeholder={"City"}
+          />
+        )}
+        name="city"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      {errors.firstName && <Text style={styles.errorText}>This is required.</Text>}
       <TextInput
         value={phoneNumber}
         onChangeText={(e) => setPhoneNumber(e)}
@@ -45,7 +78,7 @@ const AddScreen = () => {
       <Text>(future add)Products Used</Text>
       <Text>(future add)Total Spent</Text>
       <Text>(future add)Formula</Text>
-      <Button onPress={addClientHandler} title="Add Client"></Button>
+      <Button style={styles.button} onPress={handleSubmit(onSubmit)} title="Add Client"></Button>
     </View>
   );
 };
@@ -60,12 +93,20 @@ const styles = StyleSheet.create({
   text: {
     textAlign: "center",
     fontSize: 24,
+    color: "white",
+  },
+  button: {
+    color: "white",
+    backgroundColor: "white",
   },
   textInput: {
     backgroundColor: "white",
     height: "8%",
     width: "90%",
     marginVertical: 5,
+  },
+  errorText: {
+    color: "red",
   },
 });
 
